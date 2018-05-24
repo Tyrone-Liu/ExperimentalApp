3ca7a2358491c1648114a2a9df0693b099e95784
Tyrone I. Liu <q1342893971@gmail.com>
2018-05-24 20:17:05 +0800

    [feat] change 'TextureView' in MainActivity to 'RatioChangeableTextureView'
    'RatioChangeableTextureView' add a method 'setAspectRatio' to set aspect
    ratio of TextureView appropriately.  Therefore the view will not be
    stretched.

---
+++ app/src/main/java/com/tyroneil/experimentalapp/RatioChangeableTextureView.java
@@ -0,0 +1,55 @@
+package com.tyroneil.experimentalapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Size;
+import android.view.TextureView;
+
+public class RatioChangeableTextureView extends TextureView {
+
+    private int ratioWidth = 0;
+    private int ratioHeight = 0;
+
+    /**
+     * Three default constructor
+     */
+    public RatioChangeableTextureView(Context context) {
+        super(context);
+    }
+    public RatioChangeableTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public RatioChangeableTextureView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    /**
+     * Set the aspect ratio of TextureView, will take effect through the overrode {@code onMeasure()} method.
+     * The scale of ratio value does not matter (4:3 is the same with 8:6).
+     *
+     * @param ratioWidth the width of ratio
+     * @param ratioHeight the height of ratio
+     */
+    public void setAspectRatio(int ratioWidth, int ratioHeight) {
+        this.ratioWidth = ratioWidth;
+        this.ratioHeight = ratioHeight;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        int height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+
+        if (ratioWidth == 0 || ratioHeight == 0) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (((float) ratioWidth / ratioHeight) < ((float) width / height)) {
+                setMeasuredDimension(height * ratioWidth / ratioHeight, height);
+            } else {
+                setMeasuredDimension(width, width * ratioHeight / ratioWidth);
+            }
+        }
+    }
+}
