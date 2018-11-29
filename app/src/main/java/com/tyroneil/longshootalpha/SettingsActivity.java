6f918e0cd1a183c82401c99ed336b32f8464db09
Tyrone I. Liu <q1342893971@gmail.com>
2018-11-30 05:58:09 +0800

    [refactor] replace ActionBar in SettingsActivity with Toolbar

--- app/src/main/java/com/tyroneil/longshootalpha/SettingsActivity.java
+++ app/src/main/java/com/tyroneil/longshootalpha/SettingsActivity.java
@@ -1,17 +1,43 @@
 package com.tyroneil.longshootalpha;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar_activity_settings = (Toolbar) findViewById(R.id.toolbar_activity_settings);
+        setSupportActionBar(toolbar_activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.preference_content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_about:
+                // TODO: show 'about app' information
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 }
