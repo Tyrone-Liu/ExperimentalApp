f398282ef5ebf8c5dc14908db47d86dcca3fad57
Tyrone I. Liu <q1342893971@gmail.com>
2018-12-28 20:54:46 +0800

    [refactor] alternative way to set EditTextPreference input type
    HIGH FIVE!
    Thanks to an update to `androidx.preference.EditTextPreference`, now I
    can set input type through `EditTextPreference.OnBindEditTextListener`.

--- app/src/main/java/com/tyroneil/longshootalpha/SettingsFragment.java
+++ app/src/main/java/com/tyroneil/longshootalpha/SettingsFragment.java
@@ -1,6 +1,11 @@
 package com.tyroneil.longshootalpha;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -8,5 +13,14 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        EditTextPreference preference_preview_exposure_time_limit_value = (EditTextPreference) findPreference("preference_preview_exposure_time_limit_value");
+        preference_preview_exposure_time_limit_value.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+            @Override
+            public void onBindEditText(@NonNull EditText editText) {
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            }
+        });
+
     }
 }
